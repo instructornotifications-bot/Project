b3fabb9842fa9eb9f3b09b2a9dadf1e89b4506bb
instructornotifications <instructornotifications@gmail.com>
2025-08-15 01:31:33 +0000

    Complete web app deploy script

--- shell.sh
+++ shell.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 echo "deploying web apps on NGINX"
 
+echo "update the system"
+sudo apt update -y
+
+echo "install utilites"
+sudo apt install -y zip unzip
+
+echo "install nginx"
+sudo apt install -y nginx
+
+echo "remove old files"
+sudo rm  -r /var/www/html
+
+echo "deploy login app"
+sudo git clone https://github.com/instructornotifications-bot/Project.git /var/www/html
+
+echo "deployed web apps on nginx"
